79d20e638c31fc2ad642917d7d154dc6ac023ab3
Lilian Carvalho <kleidk74@gmail.com>
2023-08-23 22:44:56 -0300

    creating and finishing about me and skills

--- js/scroll.js
+++ js/scroll.js
@@ -4,13 +4,21 @@ const sr = ScrollReveal ({
     reset: true
 });
 
-sr.reveal('.logo',{delay:200, origin: 'left'});
+// sr.reveal('.logo',{delay:200, origin: 'left'});
 // sr.reveal('.navbar',{delay:400, origin: 'top'});
 // sr.reveal('.menu-btn',{delay:520, origin: 'right'});
 
-sr.reveal('.home-text span',{delay:600, origin: 'top'});
-sr.reveal('.home-text h1',{delay:680, origin: 'left'});
-sr.reveal('.home-text p',{delay:750, origin: 'right'});
-sr.reveal('.main-bnt',{delay:860, origin: 'left'});
-sr.reveal('.share',{delay:950, origin: 'bottom'});
-sr.reveal('.home-img',{delay:1000, origin: 'right'});
+sr.reveal('.home-text span',{delay:580, origin: 'top'});
+sr.reveal('.home-text h1',{delay:660, origin: 'left'});
+sr.reveal('.home-text p',{delay:730, origin: 'right'});
+sr.reveal('.main-bnt',{delay:840, origin: 'left'});
+sr.reveal('.share',{delay:930, origin: 'bottom'});
+sr.reveal('.home-img',{delay:980, origin: 'right'});
+
+sr.reveal('.about-mee',{delay:300, origin: 'top'});
+sr.reveal('.container-about-me h4',{delay:310, origin: 'top'});
+sr.reveal('.container-about-me h2',{delay:320, origin: 'right'});
+sr.reveal('.container-about-me p',{delay:330, origin: 'left'});
+
+sr.reveal('.container-skills h2',{delay:320, origin: 'top'});
+sr.reveal('.skills',{delay:350, origin: 'left'});
