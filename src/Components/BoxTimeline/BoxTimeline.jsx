df9f1d4a550cca4527c9c24fec916c67aea5ad72
Lilian Carvalho <kleidk74@gmail.com>
2026-03-26 22:31:11 -0300

    feat: - reajustes na parte de destaque - reajustes no header - reajustes no background - novos componentes - conteúdo escrito novo - nova seção de contato

--- src/Components/BoxTimeline/BoxTimeline.jsx
+++ src/Components/BoxTimeline/BoxTimeline.jsx
@@ -1,10 +1,14 @@
 import ScrollRevealComponent from "../ScrollReveal/ScrollReveal";
 import "./BoxTimeline.css";
+import { useTheme } from "../../ThemeContext";
 
 // eslint-disable-next-line react/prop-types
 const BoxTimeline = ({TitleTimeline, SubtitleTimeline, DescriptionTimeline}) => {
+
+    const { theme } = useTheme();
+
     return(
-        <div className="box-timeline">
+        <div className="box-timeline" style={{ background: theme.NavBackground, color: theme.text,  border: theme.border }}>
                 <ScrollRevealComponent />
                 <h4>{TitleTimeline}</h4>
                 <h3>{SubtitleTimeline}</h3>
